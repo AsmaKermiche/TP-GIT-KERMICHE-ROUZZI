20ae760d154088cd723881b7fcd58cebc344e5a9
chaima <ic_rouzzi@esi.dz>
2022-01-20 16:32:24 +0100

    Création d'un trajet

---
+++ src/main/java/com/telly/service/BusService.java
@@ -0,0 +1,29 @@
+package com.telly.service;
+import java.sql.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import com.telly.dao.Bus;
+import com.telly.dao.BusDao;
+
+import java.util.List;
+
+@Service("busService")
+public class BusService {
+	
+	@Autowired
+	private BusDao busDao;
+	
+	public void create(Bus bus){
+		busDao.create(bus);
+	}
+	
+	public List<Bus> getCity(String leaveFrom, String goingTo, Date dateLeave, Date dateReturn){
+		return busDao.getCity(leaveFrom, goingTo, dateLeave, dateReturn);
+	}
+	
+	
+	
+
+}
